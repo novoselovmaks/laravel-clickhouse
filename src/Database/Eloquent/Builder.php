9af35be887644d06648b76fa532e9a7f9e8fd9a2
novoselovmaks <0887404@gmail.com>
2023-10-10 18:35:13 +0300

    Update Builder.php

--- src/Database/Eloquent/Builder.php
+++ src/Database/Eloquent/Builder.php
@@ -433,6 +433,52 @@ class Builder
         return Str::contains($name, '.') && Str::startsWith($name, $relation.'.');
     }
 
+
+   /**
+     * Chunk the results of the query.
+     *
+     * @param  int  $count
+     * @param  callable  $callback
+     * @return bool
+     */
+    public function chunk($count, callable $callback)
+    {
+        dump('кол-во до ' . $this->count());
+        $this->enforceOrderBy();
+        dump('кол-во после ' . $this->count());
+
+
+        $page = 1;
+
+        do {
+            // We'll execute the query for the given page and get the results. If there are
+            // no results we can just break and return from here. When there are results
+            // we will call the callback with the current chunk of these results here.
+            $results = $this->forPage($page, $count)->get();
+
+            $countResults = $results->count();
+
+            if ($countResults == 0) {
+                break;
+            }
+
+            // On each chunk result set, we will pass them to the callback and then let the
+            // developer take care of everything within the callback, which allows us to
+            // keep the memory low for spinning through large result sets for working.
+            if ($callback($results, $page) === false) {
+                return false;
+            }
+
+            unset($results);
+
+            $page++;
+        } while ($countResults == $count);
+
+        return true;
+    }
+
+    
+    
     /**
      * Chunk the results of a query by comparing numeric IDs.
      *
